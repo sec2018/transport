34b6c1790db53b111875ddbb873d35452cdd3597
sec2018 <sec2018ilearn@163.com>
2018-10-07 22:47:26 +0800

    打包成war

--- src/main/java/com/example/transport/CityTransportApplication.java
+++ src/main/java/com/example/transport/CityTransportApplication.java
@@ -2,11 +2,29 @@ package com.example.transport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+//@SpringBootApplication
+//public class CityTransportApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(CityTransportApplication.class, args);
+//	}
+//}
+
+//for war
+@EnableCaching
 @SpringBootApplication
-public class CityTransportApplication {
-
-	public static void main(String[] args) {
+@EnableTransactionManagement
+public class CityTransportApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CityTransportApplication.class);
+	}
+	public static void main( String[] args ){
 		SpringApplication.run(CityTransportApplication.class, args);
 	}
 }
