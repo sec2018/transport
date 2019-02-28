5b8b8c73d01855e4b7979db263791fb09048abc1
sec2018 <sec2018ilearn@163.com>
2019-02-28 21:36:53 +0800

    home21

--- src/main/java/com/example/transport/wxpaytest/WeiXinPaymentController.java
+++ src/main/java/com/example/transport/wxpaytest/WeiXinPaymentController.java
@@ -19,10 +19,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -35,7 +35,7 @@ public class WeiXinPaymentController{
     private static final long serialVersionUID = 1L;
     private final String mch_id = Constant.WX_SHOP_ID;//商户号
     private final String spbill_create_ip = "192.168.100.100";//终端IP
-    private final String notify_url = "192.168.100.100:8882/transport/api/paycallback";//通知地址
+    private final String notify_url = "https://wzjshuye.cn:8882/transport/api/paycallback";//通知地址
     private final String trade_type = "JSAPI";//交易类型
     private final String url = "https://api.mch.weixin.qq.com/pay/unifiedorder";//统一下单API接口链接
     private final String key = Constant.WX_SHOP_KEY; // 商户支付密钥
@@ -238,17 +238,75 @@ public class WeiXinPaymentController{
      * 预支付时填写的 notify_url ，支付成功后的回调接口
      * @param request
      */
-    @ApiOperation(value = "支付回调接口", notes = "支付回调接口")
+//    @ApiOperation(value = "支付回调接口", notes = "支付回调接口")
+//    @RequestMapping(value="paycallback")
+//    @ResponseBody
+//    public void paycallback(HttpServletRequest request) {
+//        try {
+//            Map<String, Object> dataMap = XmlUtil.parseXML(request);
+//            System.out.println(JSON.toJSONString(dataMap));
+//            //{"transaction_id":"4200000109201805293331420304","nonce_str":"402880e963a9764b0163a979a16e0002","bank_type":"CFT","openid":"oXI6G5Jc4D44y2wixgxE3OPwpDVg","sign":"262978D36A3093ACBE4B55707D6EA7B2","fee_type":"CNY","mch_id":"1491307962","cash_fee":"10","out_trade_no":"14913079622018052909183048768217","appid":"wxa177427bc0e60aab","total_fee":"10","trade_type":"JSAPI","result_code":"SUCCESS","time_end":"20180529091834","is_subscribe":"N","return_code":"SUCCESS"}
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    /**
+     * 支付回调
+     * @param request
+     * @param response
+     * @throws InterruptedException
+     */
+//    @SuppressWarnings({ "unchecked", "rawtypes" })
     @RequestMapping(value="paycallback")
-    @ResponseBody
-    public void paycallback(HttpServletRequest request) {
-        try {
-            Map<String, Object> dataMap = XmlUtil.parseXML(request);
-            System.out.println(JSON.toJSONString(dataMap));
-            //{"transaction_id":"4200000109201805293331420304","nonce_str":"402880e963a9764b0163a979a16e0002","bank_type":"CFT","openid":"oXI6G5Jc4D44y2wixgxE3OPwpDVg","sign":"262978D36A3093ACBE4B55707D6EA7B2","fee_type":"CNY","mch_id":"1491307962","cash_fee":"10","out_trade_no":"14913079622018052909183048768217","appid":"wxa177427bc0e60aab","total_fee":"10","trade_type":"JSAPI","result_code":"SUCCESS","time_end":"20180529091834","is_subscribe":"N","return_code":"SUCCESS"}
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void wxNotify(HttpServletRequest request,HttpServletResponse response) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader((ServletInputStream)request.getInputStream()));
+        String line = null;
+        StringBuilder sb = new StringBuilder();
+        while((line = br.readLine()) != null){
+            sb.append(line);
         }
+        br.close();
+        //sb为微信返回的xml
+        String notityXml = sb.toString();
+        String resXml = "";
+        System.out.println("接收到的报文：" + notityXml);
+
+        Map map = com.example.transport.sdk.PayUtil.doXMLParse(notityXml);
+
+        String returnCode = (String) map.get("return_code");
+        if("SUCCESS".equals(returnCode)){
+            //验证签名是否正确
+            Map<String, String> validParams = PayUtil.paraFilter(map);  //回调验签时需要去除sign和空值参数
+            String validStr = PayUtil.createLinkString(validParams);//把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串
+            String sign = PayUtil.sign(validStr, key, "utf-8").toUpperCase();//拼装生成服务器端验证的签名
+            // 因为微信回调会有八次之多,所以当第一次回调成功了,那么我们就不再执行逻辑了
+
+            //根据微信官网的介绍，此处不仅对回调的参数进行验签，还需要对返回的金额与系统订单的金额进行比对等
+            if(sign.equals(map.get("sign"))){
+                /**此处添加自己的业务逻辑代码start**/
+                // bla bla bla....
+                Date datetime = new Date();
+                System.out.println("我自己的业务逻辑！");
+                /**此处添加自己的业务逻辑代码end**/
+                //通知微信服务器已经支付成功
+                resXml = "<xml>" + "<return_code><![CDATA[SUCCESS]]></return_code>"
+                        + "<return_msg><![CDATA[OK]]></return_msg>" + "</xml> ";
+            } else {
+                System.out.println("微信支付回调失败!签名不一致");
+            }
+        }else{
+            resXml = "<xml>" + "<return_code><![CDATA[FAIL]]></return_code>"
+                    + "<return_msg><![CDATA[报文为空]]></return_msg>" + "</xml> ";
+        }
+        System.out.println(resXml);
+        System.out.println("微信支付回调数据结束");
+
+        BufferedOutputStream out = new BufferedOutputStream(
+                response.getOutputStream());
+        out.write(resXml.getBytes());
+        out.flush();
+        out.close();
     }
 
 
